e6269078f0b2de07f9e4b5c78e1012e7384a51ff
narayan6943 <nayannarayandwivedi@gmail.com>
2025-12-15 18:51:16 +0530

    c3

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 const nextConfig = {
   // Disable React Strict Mode to stabilize Fast Refresh
   reactStrictMode: false,
-  output: 'export',
+  output: process.env.NODE_ENV === 'development' ? undefined : 'export',
 
   // Fix path aliases for both server and client
   webpack: (config, { dev, isServer }) => {
